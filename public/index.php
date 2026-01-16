c97db0e35da0069e2eee0703930c801a35aea661
Willy Bahuaud <rouge13scompany@hotmail.com>
2026-01-17 00:47:31 +0100

    Test auto-deploy: add version field

--- public/index.php
+++ public/index.php
@@ -1,7 +1,9 @@
 <?php
 // Placeholder - sera remplacé par Laravel
+header('Content-Type: application/json');
 echo json_encode([
     'app' => 'Plugin Hub',
     'status' => 'ok',
+    'version' => '0.1.0',
     'message' => 'API en cours de développement'
 ]);
